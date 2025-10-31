fd1b79a1a3cf9842a2701474a0530d99f66052d4
Brian Kominick <brian.kominick@tutanota.com>
2025-10-31 13:48:24 -0400

    Updated config for user site. Added build

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import generatePostsPlugin from "./scripts/vitePluginGeneratePosts";
 import tailwind from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/",
   plugins: [react(), generatePostsPlugin(), tailwind()],
-  // ✅ Use `mode` instead of `process.env.NODE_ENV`
-  base: mode === "production" ? "/my-repo-name/" : "/",
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
       "@public": path.resolve(__dirname, "./public"),
     },
   },
-}));
+});
