6885ddb61689412f877fd2e79b868f83aa8e6aa9
Brian Kominick <brian.kominick@tutanota.com>
2025-10-31 13:40:51 -0400

    update vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import generatePostsPlugin from "./scripts/vitePluginGeneratePosts";
 import tailwind from "@tailwindcss/vite";
-import path from 'path';
+import path from "path";
 
-// Vite configuration
-const isProd = process.env.NODE_ENV === "production";
-
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), generatePostsPlugin(), tailwind()],
-  base: process.env.NODE_ENV === "production" ? "/my-repo-name/" : "/",
-  resolve:
-    {alias: {
-                '@components': path.resolve(__dirname, './src/components'),
-                '@public': path.resolve(__dirname, './public'),
-            },
-          }
-});
+  // ✅ Use `mode` instead of `process.env.NODE_ENV`
+  base: mode === "production" ? "/my-repo-name/" : "/",
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@public": path.resolve(__dirname, "./public"),
+    },
+  },
+}));
